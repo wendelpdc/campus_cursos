4a84e2a970c21f4f935c4bf9852a286b411943c6
Wendel Victor <wendelvictor04@gmail.com>
2025-11-17 17:19:59 -0300

    refactor: alter campus.py

--- campus.py
+++ campus.py
@@ -7,10 +7,8 @@ class Campus:
         
     def add_curso(self, curso):
         if curso not in self.cursos:
-            codigo_curso = int(input("Código do curso: "))
-            nome_curso = input("Nome do curso: ")
-            self.cursos.append(codigo_curso, nome_curso)
-            print(f"O curso {curso} foi adicionado ao campus {self.nome} com sucesso!")
+            self.cursos.append(curso)
+            print(f"O curso '{curso}' foi adicionado ao campus {self.nome}.")
         else:
             print(f"O curso {curso} já existe no campus {self.nome}.")
         
@@ -23,18 +21,45 @@ class Campus:
             print("Não há curso cadastrado no campus {self.nome}.")
             
 def menu_interativo():
-    print("***************")
-    print("* MENU CAMPUS *")
-    print("***************")
+    campus = None
+    
+    while True:
+        print("***************")
+        print("* MENU CAMPUS *")
+        print("***************")
 
-    print("1 - Criar campus")
-    print("2 - Adicionar curso")
-    print("3 - Listar cursos")
-    print("4 - Sair")
-    opcao = input("Escolha uma opção: ")
+        print("1 - Criar campus")
+        print("2 - Adicionar curso")
+        print("3 - Listar cursos")
+        print("4 - Sair")
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == '1':
+            codigo = int(input("Dgite o código do campus: "))
+            nome = input("Digite o nome do campus: ")
+            endereco = input("Digite o endereço do campus: ")
+            campus = Campus(codigo, nome, endereco)
+            print(f"Campus {nome} criado com sucesso.")
+        
+        elif opcao == '2':
+            if campus is None:
+                print("Crie um campus primeiro.")
+            else:
+                curso = input("Digite o nome do curso para que possa ser adicionado: ")
+                campus.add_curso(curso)
+        
+        elif opcao == '3':
+            if campus is None:
+                print("Crie um campus primeiro.")
+            else:
+                campus.listar_cursos
+    
+        elif opcao == '4':
+            print("Saindo...")
+            break
 
 if __name__ == "__main__":
     menu_interativo()
 
-            
+                
         
