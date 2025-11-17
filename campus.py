7552b07d5f09e691d1cee93186a21dc2145faace
Wendel Victor <wendelvictor04@gmail.com>
2025-11-17 15:01:17 -0300

    refactor: alter campus.py

--- campus.py
+++ campus.py
@@ -14,7 +14,27 @@ class Campus:
         else:
             print(f"O curso {curso} já existe no campus {self.nome}.")
         
+    def listar_cursos(self):
+        if self.cursos:
+            print(f"Cursos presentes no campus {self.nome}: ")
+            for curso in self.cursos:
+                print(f"{curso}")
+        else:
+            print("Não há curso cadastrado no campus {self.nome}.")
             
+def menu_interativo():
+    print("***************")
+    print("* MENU CAMPUS *")
+    print("***************")
+
+    print("1 - Criar campus")
+    print("2 - Adicionar curso")
+    print("3 - Listar cursos")
+    print("4 - Sair")
+    opcao = input("Escolha uma opção: ")
+
+if __name__ == "__main__":
+    menu_interativo()
 
             
         
