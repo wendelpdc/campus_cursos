2bd39265ae18b0b08a585e8175e45988e997b919
Wendel Victor <wendelvictor04@gmail.com>
2025-11-19 08:59:50 -0300

    refactor: alter campus.py

--- campus.py
+++ campus.py
@@ -4,21 +4,6 @@ class Campus:
         self.nome = nome
         self.endereco = endereco
         self.cursos = []
-        
-    def add_curso(self, curso):
-        if curso not in self.cursos:
-            self.cursos.append(curso)
-            print(f"O curso '{curso}' foi adicionado ao campus {self.nome}.")
-        else:
-            print(f"O curso {curso} já existe no campus {self.nome}.")
-        
-    def listar_cursos(self):
-        if self.cursos:
-            print(f"Cursos presentes no campus {self.nome}, até o devido momento: ")
-            for curso in self.cursos:
-                print(f"{curso}")
-        else:
-            print("Não há curso cadastrado no campus {self.nome}.")
             
 def menu_interativo():
     campus = None
